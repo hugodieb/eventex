03c17e6df7b7062be5857707cf4a5ef426a3c319
hugodieb <hugodieb.hd@gmail.com>
2017-07-11 12:09:56 -0300

    Preenchimento opcional telefone ou email do formulario

--- eventex/subscriptions/forms.py
+++ eventex/subscriptions/forms.py
@@ -11,9 +11,8 @@ def validate_cpf(value):
 class SubscriptionForm(forms.Form):
 	name = forms.CharField(label='Nome')
 	cpf = forms.CharField(label='CPF', max_length=11, validators=[validate_cpf])
-	email = forms.EmailField(label='Email')
-	phone = forms.CharField(label='Telefone')
-
+	email = forms.EmailField(label='Email', required=False)
+	phone = forms.CharField(label='Telefone', required=False)
 
 	#Inicia nome com letra maiuscula
 	def clean_name(self):
@@ -22,3 +21,9 @@ class SubscriptionForm(forms.Form):
 		#words = [w.capitalize() for w in name.split()]
 		# return ' '.join(words)
 		return self.cleaned_data['name']
+
+	#Retorno depois que validou o formulario
+	def clean(self):
+		if not self.cleaned_data.get('email') and not self.cleaned_data.get('phone'):
+			raise ValidationError('Informe email ou telefone.')
+		return self.cleaned_data
--- eventex/subscriptions/tests/test_form_subscription.py
+++ eventex/subscriptions/tests/test_form_subscription.py
@@ -23,8 +23,24 @@ class SubscriptionFormTest(TestCase):
         self.assertFormErrorCode(form, 'cpf', 'cpf_length')
 
     def test_name_must_be_name_capitalized(self):
+        """Name must be capitalized"""
         form = self.make_validated_form(name='HUGO dieb')
         self.assertEqual('Hugo Dieb', form.cleaned_data['name'])
+
+    def test_email_is_optional(self):
+        """Email is optional"""
+        form = self.make_validated_form(email='')
+        self.assertFalse(form.errors)
+
+    def test_phone_is_optional(self):
+        """Phone is optional"""
+        form = self.make_validated_form(phone='')
+        self.assertFalse(form.errors)
+
+    def test_must_inform_email_or_phone(self):
+        """Email or phone is optional, but one must be informed."""
+        form = self.make_validated_form(email='', phone='')
+        self.assertListEqual(['__all__'], list(form.errors))
 
     def assertFormErrorCode(self, form, field, code):
         errors = form.errors.as_data()
