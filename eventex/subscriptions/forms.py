0b0c613d5a1eaac9fa1e7d2fe83eafbb0726a8ae
hugodieb <hugodieb.hd@gmail.com>
2017-07-12 09:50:10 -0300

    Refactor form para modelForm

--- eventex/subscriptions/forms.py
+++ eventex/subscriptions/forms.py
@@ -1,14 +1,10 @@
 from django import forms
 from django.core.exceptions import ValidationError
+from eventex.subscriptions.models import Subscription
+from eventex.subscriptions.validators import validate_cpf
 
 
-def validate_cpf(value):
-	if not value.isdigit():
-		raise ValidationError('CPF deve conter apenas números', 'cpf_digits')
-	if len(value) != 11:
-		raise ValidationError('CPF deve ter 11 digitos', 'cpf_length')
-
-class SubscriptionForm(forms.Form):
+class SubscriptionFormOld(forms.Form):
 	name = forms.CharField(label='Nome')
 	cpf = forms.CharField(label='CPF', max_length=11, validators=[validate_cpf])
 	email = forms.EmailField(label='Email', required=False)
@@ -24,6 +20,29 @@ class SubscriptionForm(forms.Form):
 
 	#Retorno depois que validou o formulario
 	def clean(self):
+		if not self.cleaned_data.get('email') and not self.cleaned_data.get('phone'):
+			raise ValidationError('Informe email ou telefone.')
+		return self.cleaned_data
+
+
+class SubscriptionForm(forms.ModelForm):
+
+	class Meta:
+		model = Subscription
+		fields = ['name', 'cpf', 'email', 'phone']
+
+	# Inicia nome com letra maiuscula
+	def clean_name(self):
+		name = self.cleaned_data['name']
+		self.cleaned_data['name'] = name.title()
+		# words = [w.capitalize() for w in name.split()]
+		# return ' '.join(words)
+		return self.cleaned_data['name']
+
+	# Retorno depois que validou o formulario
+	def clean(self):
+		self.cleaned_data = super().clean()
+
 		if not self.cleaned_data.get('email') and not self.cleaned_data.get('phone'):
 			raise ValidationError('Informe email ou telefone.')
 		return self.cleaned_data
