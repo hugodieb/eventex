e2092794c4a59fa7033a622f8776c87baba08558
hugodieb <hugodieb.hd@gmail.com>
2017-07-13 12:41:28 -0300

    Teste de inclusão de palestrante

--- eventex/core/views.py
+++ eventex/core/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
-# Create your views here.
+from eventex.core.models import Speaker
+
 
 def home(request):
     speakers = [
@@ -8,3 +9,8 @@ def home(request):
         {'name': 'Allan Turing ', 'photo': 'http://hbn.link/turing-pic'}
     ]
     return render(request, 'index.html', {'speakers': speakers})
+
+def speaker_detail(request, slug):
+    speaker = get_object_or_404(Speaker, slug=slug)
+
+    return render(request, 'core/speaker_detail.html', {'speaker':speaker})
