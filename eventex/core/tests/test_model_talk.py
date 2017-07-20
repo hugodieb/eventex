8525ed699c73bf060b47c01b02c781a5bdc71456
hugodieb <hugodieb.hd@gmail.com>
2017-07-20 10:19:19 -0300

    Talk

--- eventex/core/tests/test_model_talk.py
+++ eventex/core/tests/test_model_talk.py
@@ -5,8 +5,8 @@ class TalkModelTest(TestCase):
     def setUp(self):
         self.talk = Talk.objects.create(
             title = 'Título da palestra',
-            start = '10:00',
-            description = 'Descrição da palestra'
+            #start = '10:00',
+            #description = 'Descrição da palestra'
         )
 
     def test_create(self):
@@ -20,3 +20,22 @@ class TalkModelTest(TestCase):
             website='hugo@dieb.net'
         )
         self.assertEqual(1, self.talk.speakers.count())
+
+    def test_description_blank(self):
+        field = Talk._meta.get_field('description')
+        self.assertTrue(field.blank)
+
+    def test_speakers_blank(self):
+        field = Talk._meta.get_field('speakers')
+        self.assertTrue(field.blank)
+
+    def test_start_blank(self):
+        field = Talk._meta.get_field('start')
+        self.assertTrue(field.blank)
+
+    def test_start_null(self):
+        field = Talk._meta.get_field('start')
+        self.assertTrue(field.null)
+
+    def test_str(self):
+        self.assertEqual('Título da palestra', str(self.talk))
--- eventex/core/tests/test_view_talk_list.py
+++ eventex/core/tests/test_view_talk_list.py
@@ -44,3 +44,9 @@ class TalkListGet(TestCase):
         for key in variables:
             with self.subTest():
                 self.assertIn(key, self.response.context)
+
+class TalkListGetEmpty(TestCase):
+    def test_get_empty(self):
+        response = self.client.get(r('talk_list'))
+        self.assertContains(response, 'Ainda não existem palestras de manhã')
+        self.assertContains(response, 'Ainda não existem palestras á tarde')
