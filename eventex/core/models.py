8d5d8e67c098a80cb9de592888f7e40eedceee39
hugodieb <hugodieb.hd@gmail.com>
2017-08-10 09:31:43 -0300

    Deletado tabela CourseOld e refatorado Course e Talk

--- eventex/core/models.py
+++ eventex/core/models.py
@@ -44,7 +44,7 @@ class Contact(models.Model):
     def __str__(self):
         return self.value
 
-class Activity(models.Model):
+class Talk(models.Model):
     title = models.CharField('título', max_length=200)
     start = models.TimeField('início', blank=True, null=True)
     description = models.TextField('descrição', blank=True)
@@ -53,25 +53,17 @@ class Activity(models.Model):
     objects = PeriodManager()
 
     class Meta:
-        abstract = True
+        ordering = ['start']
         verbose_name_plural = 'palestras'
         verbose_name = 'palestra'
 
     def __str__(self):
         return self.title
 
-class Talk(Activity):
-    pass
-
-class CourseOld(Activity):
-    slots = models.IntegerField()
-
-    class Meta:
-        verbose_name_plural = 'cursos'
-        verbose_name = 'curso'
-
 class Course(Talk):
     slots = models.IntegerField()
+
+    objects = PeriodManager()
 
     class Meta:
         verbose_name_plural = 'cursos'
