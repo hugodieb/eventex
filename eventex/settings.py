215d79fc5c047cb9fdd1f8c19da5576162e51a2c
hugodieb <hugodieb.hd@gmail.com>
2017-08-30 12:39:12 -0300

    ajuste postgres para rodar testes no travis

--- eventex/settings.py
+++ eventex/settings.py
@@ -84,18 +84,18 @@ WSGI_APPLICATION = 'eventex.wsgi.application'
 
 #default_dburl = 'sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite3')
 #default_dburl='postgres://eventex:eventex@localhost/eventex_dev'
-#default_dburl=config('DB_URL')
+default_dburl=config('DB_URL')
 
 DATABASES = {
-#    'default': config('DATABASE_URL', default=default_dburl, cast=dburl),
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': config('DB_NAME'),
-        'USER': config('DB_USER'),
-        'PASSWORD': config('DB_PASSWORD'),
-        'HOST': config('DB_HOST'),
-        'PORT': config('DB_PORT'),
-    }
+    'default': config('DATABASE_URL', default=default_dburl, cast=dburl),
+#    'default': {
+#        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#        'NAME': config('DB_NAME'),
+#        'USER': config('DB_USER'),
+#        'PASSWORD': config('DB_PASSWORD'),
+#        'HOST': config('DB_HOST'),
+#        'PORT': config('DB_PORT'),
+#    }
 }
 
 
